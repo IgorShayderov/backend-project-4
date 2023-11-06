da5ca24e4e42d20cb3ce3806ea28b23d0f817d0c
Igor <flashmob123@gmail.com>
2023-11-06 15:43:32 +0200

    feat: create assets directory

--- src/index.js
+++ src/index.js
@@ -1,19 +1,54 @@
 import axios from 'axios';
 import fs from 'fs/promises';
 import path from 'path';
+import * as cheerio from 'cheerio';
+
+// найти ссылки в html
+// создать директорию для файлов
+// скачать изображения
+// поменять названия изображениям
+// поправить тесты
+
+const tranformFilename = (filename) => filename.replace(/[^\w]+/g, '-');
 
 const pageLoader = async (url, options = {}) => {
   const pageURL = new URL(url);
+  const fileName = tranformFilename(`${pageURL.host}${pageURL.pathname}`);
+  const outputDir = options.output ?? process.cwd();
 
   new Promise((resolve) => {
     resolve(axios.get(pageURL));
   })
     .then(({ data }) => {
-      const fileName = `${pageURL.host}${pageURL.pathname}`.replace(/[^\w]+/g, '-');
-      const outputDir = options.output ?? process.cwd();
+      const $data = cheerio.load(data);
 
-      return fs.writeFile(path.join(outputDir, `${fileName}.html`), data);
+      return fs.writeFile(path.join(outputDir, `${fileName}.html`), data)
+        .then(() => $data);
     })
+    .then(($data) => {
+      const $images = $data('img');
+      const imagesUrls = $images
+        .map((index, image) => $data(image).attr('src'))
+        .toArray();
+
+      $images.attr('src', (index, attr) => {
+        console.log({ attr });
+
+        return '';
+      });
+
+      const filesDirname = path.join(outputDir, `${fileName}_files`);
+
+      return fs.readdir(filesDirname)
+        .then(() => Promise.resolve(), () => fs.mkdir(filesDirname))
+        .then(() => imagesUrls);
+    })
+    .then((imagesUrls) => Promise.allSettled([
+      imagesUrls.map((imageSrc) => axios.get(imageSrc)
+        .then((image) => {
+          // fs.writeFile();
+        })),
+    ]))
     .catch((e) => {
       console.error({ e });
     });
